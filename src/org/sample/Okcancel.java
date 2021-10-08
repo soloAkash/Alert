59cd5bba23410653fac6e569c581cfb17d1d32b8
soloAkash <akashrio1234@gmail.com>
2021-10-08 16:38:33 +0530

    second commit

--- src/org/sample/Okcancel.java
+++ src/org/sample/Okcancel.java
@@ -8,21 +8,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Okcancel {
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\AKASH\\eclipse-workspace\\Alert\\driver\\chromedriver.exe");
-		WebDriver driver =new ChromeDriver();
-		driver.get("http://demo.automationtesting.in/Alerts.html");
-		driver.manage().window().maximize();
-
-		WebElement btnclick = driver.findElement(By.xpath("(//a[@class='analystic'])[2]"));
-		btnclick.click();
-		
-		WebElement btnclick2 = driver.findElement(By.xpath("//button[@class='btn btn-primary']"));
-		btnclick2.click();
-
-		Alert al = driver.switchTo().alert();
-		String text =al.getText();
-		System.out.println("ok");
-		al.accept();
+		System.out.println("hkjgkjjgw");
+		System.out.println("wdihiuwhduh");
 }
 	
 }
